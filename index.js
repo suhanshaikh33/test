4af036c73346d672d757b46a46d1aab62f6e3125
suhanshaikh33 <suhanshaikh33@gmail.com>
2021-08-11 13:49:09 +0530

    aug11v1.1

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var bar =document.querySelector(".ham-container");
-console.log(bar);
+footer=document.querySelector("footer")
+// console.log(footer);
+console.log();  
+// arr=footer.querySelectorAll("li")
+// arr.forEach((value)=>{
+//     value.setAttribute("data-aos","slide-right")
+//     value.setAttribute("data-aos-duration","2000")
+// });
+console.log(li);
 bar.addEventListener("click",()=>{
     console.log("clicked");
     document.getElementsByTagName("nav")[0].children[2].classList.toggle("none")
